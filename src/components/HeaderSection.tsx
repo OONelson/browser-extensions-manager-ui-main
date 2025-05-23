c3d9c38a0b2b416267b24a9c9b0c28aac89dfb40
Nelsonn <oonelsoncodes@gmail.com>
2025-05-23 15:55:52 +0100

    fix/ fix

--- src/components/HeaderSection.tsx
+++ src/components/HeaderSection.tsx
@@ -1,9 +1,13 @@
 import ThemeToggle from "@/components/ThemeToggle";
+import { useDarkMode } from "@/hooks/useDarkMode";
+import LogoDark from "@/icons/LogoDark";
 
 const Header: React.FC = () => {
+  const { isDarkMode } = useDarkMode();
+
   return (
     <header className="bg-neutral-50 flex justify-between items-center px-3 py-2 rounded-lg shadow-md dark:bg-gray-800">
-      <img alt="logo-icon" src="./images/logo.svg" />
+      {isDarkMode ? <LogoDark /> : <img alt="logo" src="/images/logo.svg" />}
       <ThemeToggle />
     </header>
   );
